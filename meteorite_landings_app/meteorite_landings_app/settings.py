c5e61658382b592413079c587ce533c117e84e78
r0651630 <katrien.stavrinoudis@student.ucll.be>
2020-12-11 22:43:08 +0100

    added Procfile and settings.py loop

--- meteorite_landings_app/meteorite_landings_app/settings.py
+++ meteorite_landings_app/meteorite_landings_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meteorite_landings_app.settings")
 application = get_wsgi_application()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
