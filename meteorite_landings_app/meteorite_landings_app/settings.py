d9eeadb0dddfe82b88e176b01dba544639d3bcfe
r0651630 <katrien.stavrinoudis@student.ucll.be>
2020-12-11 22:25:27 +0100

    added lines from video

--- meteorite_landings_app/meteorite_landings_app/settings.py
+++ meteorite_landings_app/meteorite_landings_app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meteorite_landings_app.settings")
+application = get_wsgi_application()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = ')%0q*09^ob=agdw$6$fjwae#v1ne(ta=qeolz378evxl03*)1^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysterious-reaches-83416.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
