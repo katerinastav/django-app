1b3f1de50200d04e779d25e079dfeeae6ae85038
r0651630 <katrien.stavrinoudis@student.ucll.be>
2020-12-12 00:09:04 +0100

    url change

--- meteorite_landings_app/meteorite_landings_app/urls.py
+++ meteorite_landings_app/meteorite_landings_app/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('api.urls')),
    # path('', include('rest_auth.urls')),
-    path('/', include('rest_auth.registration.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
 ]
